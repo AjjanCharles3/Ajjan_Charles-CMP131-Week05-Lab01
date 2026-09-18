5495fffabe4ca6c46a83e24189bf4ce7fbb61849
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-18 10:41:13 -0400

    start up

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+#Charles Ajjan
+#CMP131
+#Week5
+#Lab4
+#Even or Odd
+# Date started 09/18/2026
---
+++ temperature_category.py
@@ -0,0 +1,6 @@
+#Charles Ajjan
+#CMP131
+#Week5
+#Lab4
+#Temperature Category
+# Date started 09/18/2026
