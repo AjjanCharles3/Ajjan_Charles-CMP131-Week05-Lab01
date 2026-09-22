c3a706f335294313003d3bd805d9f003f0d95d79
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-22 10:54:09 -0400

    working

--- temperature_category.py
+++ temperature_category.py
@@ -4,14 +4,15 @@
 #Lab4
 #Temperature Category
 # Date started 09/18/2026
-temp=(input("WELCOME PLEASE ENTER THE CURRENT TEMERATURE"   ,   ))
-temp_reading= (temp)
-if (varable):  
-   Cold = 49
-   Warm=  50°F through 79°F
-   Hot= 80°F or above
+temp=(bool(input("WELCOME PLEASE ENTER THE CURRENT TEMERATURE"   ,   )))
+temp_reading= (temp)  
+Cold = ("=<49")
+Warm=  ("=>50-79")
+Hot= ("=>80+")
 print("temp", temp_reading)
-if ("temp_reading =< 49"):
-   print("Cold Remember your Jacket")
-elif("temp_reading => 50 through 79"): print("Warm outside")
-else:("temp_reading => 80 or above"), print("Hot! Maybe stay inside")
+if("temp_reading <49"): 
+   print("Cold, Remember your Jacket")
+elif ("temp_reading =>50-79"):   
+   print("Warm outside")
+else:
+   print("Hot! Maybe stay inside")
